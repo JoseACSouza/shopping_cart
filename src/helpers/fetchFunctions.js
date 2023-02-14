72fc1267345f412efd5cc2299b3352ea471a568f
José Alexandre Cordeiro Souza <alexandrecordeiro15@outlook.com>
2023-02-14 20:29:14 -0300

    completa o requisito 1 e 2

--- src/helpers/fetchFunctions.js
+++ src/helpers/fetchFunctions.js
@@ -2,6 +2,12 @@ export const fetchProduct = () => {
   // seu código aqui
 };
 
-export const fetchProductsList = () => {
+export const fetchProductsList = async (search) => {
   // seu código aqui
+  if (!search) {
+    throw new Error('Termo de busca não informado');
+  }
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${search}`);
+  const data = await response.json();
+  return data.results;
 };
--- src/tests/fetchProductsList.test.js
+++ src/tests/fetchProductsList.test.js
@@ -5,17 +5,26 @@ import computadorSearch from './mocks/search';
 // implemente seus testes aqui
 describe('Teste a função fetchProductsList', () => {
   it('fetchProductsList é uma função', () => {
-    
+    expect(typeof fetchProductsList).toBe('function');
+  });
+  it('fetch é chamado ao executar fetchProductsList', async() => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenCalled();
   });
 
-  it('fetch é chamado ao executar fetchProductsList', () => {
-
+  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', async () => {
+    await fetchProductsList('computador');
+    expect(fetch).toHaveBeenLastCalledWith('https://api.mercadolibre.com/sites/MLB/search?q=computador');
   });
 
-  it('fetch é chamado com o endpoint correto ao executar fetchProductsList', () => {
-
+  it('Teste se o retorno da função fetchProductsList com o argumento computador é uma estrutura de dados igual ao objeto computadorSearch, que já está importado no arquivo.', async () => {
+    const expected = await fetchProductsList('computador');
+    expect(Object.keys(expected[0])).toEqual(Object.keys(computadorSearch[0]));
   });
 
-  // it('...', () => {
-  // });
+  it('Teste se, ao chamar a função fetchProductsList sem argumento, retorna um erro com a mensagem: Termo de busca não informado', async () => {
+    const promise = fetchProductsList('');
+    await expect(promise).rejects.toThrow();
+  });
+
 });
